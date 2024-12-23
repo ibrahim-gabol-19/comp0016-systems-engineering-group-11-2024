034555debf74eb8f39cb8bce4eb1ea2a0b338bb8
Ibrahim G <ibrahim.gabol@protonmail.com>
2024-12-23 21:40:23 +0000

    Slight Changes

--- frontend/src/components/contentmanagementsystem/DefaultTopBar.js
+++ frontend/src/components/contentmanagementsystem/DefaultTopBar.js
@@ -38,20 +38,48 @@ const DefaultTopBar = ({ onManual, onUpload }) => {
 
       {/* Dropdown list, visible when isDropdownOpen is true */}
       <div
-        className={`absolute bg-gray-200 py-2 px-14 mt-2 rounded-lg shadow-lg z-20 left-8 w-full max-w-xs top-full transition-all duration-300  ${
+        className={`absolute bg-gray-200 py-2 px-14 mt-2 rounded-lg shadow-lg z-20 left-8 w-full max-w-xs top-full transition-all duration-200  ${
           isDropdownOpen ? "max-h-40 opacity-100" : "max-h-0 opacity-0"
         }`}
       >
         <button
-          className="justify-center w-full py-3 bg-white font-bold text-green-500  rounded-lg  active:bg-green-100 hover:outline transition duration-500 active:duration-100 mb-2"
+          className="flex flex-row justify-center w-full py-3 bg-white font-bold text-green-500  rounded-lg  active:bg-green-100 hover:outline transition duration-500 active:duration-100 mb-2"
           onClick={onManual}
         >
-          Manual
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          Create
         </button>
         <button
-          className="justify-center w-full py-3 bg-white font-bold text-green-500  rounded-lg  hover:outline active:bg-green-100 transition duration-500 active:duration-100"
+          className="flex flex-row justify-center w-full py-3 bg-white font-bold text-green-500  rounded-lg  hover:outline active:bg-green-100 transition duration-500 active:duration-100"
           onClick={onUpload}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+            />
+          </svg>
           Upload
         </button>
       </div>
