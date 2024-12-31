610a3ce03c7f03029d8b22728d13de3089f2682c
Ibrahim G <ibrahim.gabol@protonmail.com>
2024-12-31 21:56:53 +0000

    Continued Frontend Work

--- frontend/src/components/Home/SearchBar.js
+++ frontend/src/components/Home/SearchBar.js
@@ -11,33 +11,36 @@ const SearchBar = () => {
   const [searchResult, setSearchResult] = useState("");
   const [messages, setMessages] = useState([]);
 
-  const templateSearchResult = `{
-    "news": [
+  const templateSearchResult = {
+    news: [
       {
-        "title": "New Recycling Initiative Launched",
-        "description": "GreenEarth Inc. has introduced a new citywide recycling initiative aimed at reducing waste and promoting sustainability.",
-        "published": "24 Dec 2024"
+        title: "New Recycling Initiative Launched",
+        description:
+          "GreenEarth Inc. has introduced a new citywide recycling initiative aimed at reducing waste and promoting sustainability.",
+        published: "24 Dec 2024",
       },
-     ],
-    "events": [
+    ],
+    events: [
       {
-        "title": "Community Trash Pickup",
-        "description": "Join us for a local trash pickup event to help clean up our neighborhood. All supplies will be provided.",
-        "date": "30 Dec 2024",
-        "time": "9:00 AM",
-        "location": "Central Park, GreenEarth City",
-        "contact": "volunteer@greenearthinc.com"
+        title: "Community Trash Pickup",
+        description:
+          "Join us for a local trash pickup event to help clean up our neighborhood. All supplies will be provided.",
+        date: "30 Dec 2024",
+        time: "9:00 AM",
+        location: "Central Park, GreenEarth City",
+        contact: "volunteer@greenearthinc.com",
       },
       {
-        "title": "GreenEarth Social Gathering",
-        "description": "A casual get-together for employees and local residents to discuss sustainability efforts and enjoy a fun evening of eco-friendly activities.",
-        "date": "26 Dec 2024",
-        "time": "6:00 PM",
-        "location": "GreenEarth HQ, 123 Sustainability Road",
-        "contact": "events@greenearthinc.com"
+        title: "GreenEarth Social Gathering",
+        description:
+          "A casual get-together for employees and local residents to discuss sustainability efforts and enjoy a fun evening of eco-friendly activities.",
+        date: "26 Dec 2024",
+        time: "6:00 PM",
+        location: "GreenEarth HQ, 123 Sustainability Road",
+        contact: "events@greenearthinc.com",
       },
-    ]
-  }`;
+    ],
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -83,7 +86,8 @@ const SearchBar = () => {
 
   const getReply = async (userQuery, searchResult) => {
     // Because Search has not been implemented yet, I am using template JSON
-    searchResult = templateSearchResult;
+    searchResult = JSON.stringify(templateSearchResult);
+    console.log(searchResult);
 
     if (!engine) {
       console.log("Model is still loading...");
@@ -150,8 +154,8 @@ const SearchBar = () => {
         <p>Reply: {modelReply}</p>
       </div>
 
-      <div className=" h-80 w-full rounded-3xl bg-white border border-gray-300 flex flex-col ">
-        <div className="bg-green-500 w-3/4 h-2/6 ml-auto justify-end flex">
+      <div className=" h-96 w-full rounded-3xl bg-white border border-gray-300 flex flex-col ">
+        <div className="bg-green-500 w-3/4 h-1/6 ml-auto justify-end flex">
           <div className="w-5/6 h-full text-wrap truncate ">
             <p>{userQuery}</p>
           </div>
@@ -170,44 +174,66 @@ const SearchBar = () => {
             />
           </svg>
         </div>
-        <div className="w-3/4 h-4/6 bg-blue-500 flex ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6 w-1/6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
-            />
-          </svg>
-          <div className="w-5/6 h-full text-wrap truncate ">
-            <p>{modelReply}</p>
+        <div className="h-5/6">
+          <div className="w-3/4 h-1/2 bg-green-100 flex ">
+            {templateSearchResult.news.map((newsItem, index) => (
+              <div key={index} className="w-1/3 h-full bg-yellow-300">
+                <h3 className="font-bold">{newsItem.title}</h3>
+                <p>{newsItem.description}</p>
+                <span className="text-sm">{newsItem.published}</span>
+              </div>
+            ))}
+
+            {/* Loop over events */}
+            {templateSearchResult.events.map((eventItem, index) => (
+              <div key={index} className="w-1/3 h-full bg-yellow-300">
+                <h3 className="font-bold">{eventItem.title}</h3>
+                <p>{eventItem.description}</p>
+                <div className="text-sm">
+                  <p>{eventItem.date}</p>
+                  <p>{eventItem.time}</p>
+                  <p>{eventItem.location}</p>
+                  <p>{eventItem.contact}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="w-3/4 h-1/2 bg-blue-500 flex ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 w-1/6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
+              />
+            </svg>
+            <div className="w-5/6 h-full text-wrap truncate ">
+              <p>{modelReply}</p>
+            </div>
           </div>
         </div>
       </div>
 
-
-        {/* Search / Chat Input */}
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder={
-              isFocused ? "" : "When is the next volunteering event?"
-            }
-            className={`transition-all duration-300 ease-in-out p-4 pl-12 rounded-full bg-white text-black border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 ${
-              isFocused ? "h-36" : "h-12"
-            } w-full`}
-            value={userQuery}
-            onChange={(e) => setUserQuery(e.target.value)}
-            onFocus={() => setIsFocused(true)}
-            onBlur={() => setIsFocused(false)}
-          />
-        </form>
+      {/* Search / Chat Input */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder={isFocused ? "" : "When is the next volunteering event?"}
+          className={`transition-all duration-300 ease-in-out p-4 pl-12 rounded-full bg-white text-black border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+            isFocused ? "h-36" : "h-12"
+          } w-full`}
+          value={userQuery}
+          onChange={(e) => setUserQuery(e.target.value)}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
+      </form>
 
       <div className="absolute left-4 top-1/2 transform -translate-y-1/2">
         <svg
