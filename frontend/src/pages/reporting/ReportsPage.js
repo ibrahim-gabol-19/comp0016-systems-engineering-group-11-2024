b7c99e6d3f457ed3a757477df31f798c8ecc5226
Ibrahim G <ibrahim.gabol@protonmail.com>
2025-02-01 16:14:23 +0000

    + Refactored Header styling for the Reports Page

--- frontend/src/pages/reporting/ReportsPage.js
+++ frontend/src/pages/reporting/ReportsPage.js
@@ -3,13 +3,13 @@ import React, { useEffect, useState } from "react";
 import MapComponent from "../../components/reporting/MapComponent";
 import SidebarReport from "../../components/reporting/SidebarReport";
 import axios from "axios";
+import Header from "../../components/Header";
 
 const ReportsPage = () => {
   const [selectedMarker, setSelectedMarker] = useState(null);
   const [newMarker, setNewMarker] = useState(null);
   const [reports, setReports] = useState([]);
   const [filter, setFilter] = useState("open");
-
 
   const handleMarkerSelected = (item) => {
     setSelectedMarker(item);
@@ -29,8 +29,10 @@ const ReportsPage = () => {
 
       if (selectedMarker) {
         // Directly find the updated marker in the new reports
-        const updatedMarker = newReports.find((report) => report.id === selectedMarker.id);
-        setSelectedMarker(updatedMarker || null);  // Set to null if not found
+        const updatedMarker = newReports.find(
+          (report) => report.id === selectedMarker.id
+        );
+        setSelectedMarker(updatedMarker || null); // Set to null if not found
       }
       if (newMarker) {
         setNewMarker(null);
@@ -46,40 +48,43 @@ const ReportsPage = () => {
 
   useEffect(() => {
     fetchReports();
-  // eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
 
   return (
-    <div className=" h-[calc(100vh-80px)] w-screen  flex">
-      <div className="bg-[#f9f9f9]  shadow-2xl py-5 rounded-xl h-full w-2/6">
-        <SidebarReport
-          selectedMarker={selectedMarker}
-          newMarker={newMarker}
-          fetchReports={fetchReports}
-        ></SidebarReport>
-      </div>
-      <div className=" h-full w-4/6">
-        <div className="text-sm justify-center text-center font-bold pb-2">
+    <div className="h-screen flex flex-col">
+      <Header />
+      <div className="h-20"></div>
 
-          <select
-            value={filter}
-            onChange={filterChange}
-            className="px-1 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500">
-            <option value="open">Open Issues</option>
-            <option value="resolved">Resolved Issues</option>
-            <option value="closed">Closed Issues</option>
-          </select>
-
+      <div className=" h-full w-full flex">
+        <div className="bg-[#f9f9f9]  shadow-2xl py-5 rounded-xl h-full w-2/6">
+          <SidebarReport
+            selectedMarker={selectedMarker}
+            newMarker={newMarker}
+            fetchReports={fetchReports}
+          ></SidebarReport>
         </div>
-        <MapComponent
-          onMarkerSelected={handleMarkerSelected}
-          onNewMarkerSelected={handleNewMarkerSelected}
-          reports={reports}
-          newMarker={newMarker}
-          filter={filter}
-        ></MapComponent>
+        <div className=" h-full flex flex-col w-4/6 pb-9">
+          <div className="text-sm justify-center text-center font-bold pb-2">
+            <select
+              value={filter}
+              onChange={filterChange}
+              className="px-1 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            >
+              <option value="open">Open Issues</option>
+              <option value="resolved">Resolved Issues</option>
+              <option value="closed">Closed Issues</option>
+            </select>
+          </div>
+          <MapComponent
+            onMarkerSelected={handleMarkerSelected}
+            onNewMarkerSelected={handleNewMarkerSelected}
+            reports={reports}
+            newMarker={newMarker}
+            filter={filter}
+          ></MapComponent>
+        </div>
       </div>
-
     </div>
   );
 };
