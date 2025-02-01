c6636d78556fd9be010ebdaad48f3decadc81477
Ibrahim G <ibrahim.gabol@protonmail.com>
2025-02-01 14:41:24 +0000

    + Extra Sign Up page prettifying

--- frontend/src/pages/account/SignUp.js
+++ frontend/src/pages/account/SignUp.js
@@ -21,9 +21,9 @@ const SignUp = () => {
             console.error('Error signing up:', error);
         }
     };
-
+      
     return (
-        <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="h-[calc(100vh-80px)]  w-screen  bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
                 <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                     Sign up for an account
@@ -90,7 +90,7 @@ const SignUp = () => {
                         <div>
                             <button
                                 type="submit"
-                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                className="w-full  py-2 mt-2 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-600 transition duration-300"
                             >
                                 Sign up
                             </button>
