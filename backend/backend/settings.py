fd96e8875870eb18e568bcfc2ad9122834647593
Ibrahim G <ibrahim.gabol@protonmail.com>
2025-02-01 17:20:41 +0000

    + Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,6 +35,8 @@ DEBUG = False
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 # Application definition
 
 INSTALLED_APPS = [
