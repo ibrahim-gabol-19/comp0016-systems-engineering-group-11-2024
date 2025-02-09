8f0d0428f6105082362d8bb1c8051b162f0288fd
Ibrahim G <ibrahim.gabol@protonmail.com>
2025-02-09 13:38:35 +0000

    + Same bold level for sign up and login

--- frontend/src/pages/account/SignUp.js
+++ frontend/src/pages/account/SignUp.js
@@ -68,7 +68,7 @@ const SignUp = () => {
   return (
     <div className="h-[calc(100vh-80px)] w-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Sign up for an account
         </h2>
       </div>
