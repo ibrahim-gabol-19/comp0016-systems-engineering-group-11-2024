ce0d287630ae7420e1f5081ef41be5ba1fb374ce
Ibrahim Gabol <ibrahim.gabol@protonmail.com>
2025-02-26 19:13:04 +0000

    + Added API_URL to AuthContext rather than localhost

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -2,6 +2,7 @@ import { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
 const AuthContext = createContext();
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const useAuth = () => useContext(AuthContext);
 
@@ -16,7 +17,7 @@ export const AuthProvider = ({ children }) => {
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (token) {
-            axios.get('http://localhost:8000/api/auth/user/', {
+            axios.get(`${API_URL}api/auth/user/`, {
                 headers: { Authorization: `Bearer ${token}` }
             })
             .then(response => {
@@ -37,7 +38,7 @@ export const AuthProvider = ({ children }) => {
     const login = async (token) => {
         localStorage.setItem('token', token);
         try {
-            const response = await axios.get('http://localhost:8000/api/auth/user/', {
+            const response = await axios.get(`${API_URL}api/auth/user/`, {
                 headers: { Authorization: `Bearer ${token}` }
             });
             console.log("User Data After Login:", response.data);
