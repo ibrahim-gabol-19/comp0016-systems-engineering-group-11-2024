845fb5e6594762635a84b757d01cbfa1ce15a579
MohamadHabbal <modhab2005@gmail.com>
2025-02-27 23:22:45 +0000

    Fixed scroll on each message

--- frontend/src/components/reporting/SidebarReport.js
+++ frontend/src/components/reporting/SidebarReport.js
@@ -275,49 +275,49 @@ const SidebarReport = ({ selectedMarker, newMarker, fetchReports }) => {
 
 
           {/**Discussion */}
-          <div className="w-full h-3/6 overflow-auto border border-gray-300 ">
-            {selectedMarker.discussions.map((discussion, index) => (
-              <div
-                key={index}
-                className={`flex px-4 h-32 w-full min-h-16 border border-gray-200 overflow-auto ${
-                  discussion.author === "Business" ? "bg-yellow-200" : ""
-                }
-                }`}
-              >
-                {/* Profile Picture (SVG Icon) */}
-                <div className="w-1/6 h-full flex justify-center items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-10 h-10 text-gray-600"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                    />
-                  </svg>
-                </div>
-                <div className="w-5/6 overflow-y-auto break-words py-3">
-                  <p className="font-semibold">{discussion.author}</p>
-                  <p className="">{discussion.message}</p>
-                  <p className="text-gray-500 text-sm">
-                  {new Date(discussion.created_at).toLocaleString(undefined, {
-                    year: 'numeric',
-                    month: 'numeric',
-                    day: 'numeric',
-                    hour: 'numeric',
-                    minute: 'numeric',
-                    hour12: true,
-                  })}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
+{/**Discussion */}
+<div className="w-full h-auto overflow-y-auto border border-gray-300 ">
+  {selectedMarker.discussions.map((discussion, index) => (
+    <div
+      key={index}
+      className={`flex px-4 h-auto w-full min-h-16 border border-gray-200 ${
+        discussion.author === "Business" ? "bg-yellow-200" : ""
+      }`}
+    >
+      {/* Profile Picture (SVG Icon) */}
+      <div className="w-1/6 h-full flex justify-center items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-10 h-10 text-gray-600"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+          />
+        </svg>
+      </div>
+      <div className="w-5/6 break-words py-3">
+        <p className="font-semibold">{discussion.author}</p>
+        <p className="">{discussion.message}</p>
+        <p className="text-gray-500 text-sm">
+          {new Date(discussion.created_at).toLocaleString(undefined, {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+          })}
+        </p>
+      </div>
+    </div>
+  ))}
+</div>
 
           {/*New Discussion Message */}
           <div className="w-full  flex flex-col items-center justify-center h-2/6 px-3 py-3 pb-6 ">
